b1c8ec7f479ce5f2004010eb9762a99c411f5fda
west_ng <457395070@qq.com>
2024-07-16 17:20:31 +0800

    更新【广告数据报表】API Module文件

--- DataReport/AdReport/Module.php
+++ DataReport/AdReport/Module.php
@@ -3,7 +3,7 @@
  * @Author: west_ng 457395070@qq.com
  * @Date: 2024-06-15 10:54:17
  * @LastEditors: west_ng 457395070@qq.com
- * @LastEditTime: 2024-07-15 21:49:57
+ * @LastEditTime: 2024-07-16 17:12:20
  * @FilePath: /oc-marketing-api/DataReport/AdReport/Module.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,14 +14,68 @@ use core\Profile\BaseModule;
 
 class Module extends BaseModule
 {
-    public function adReportAdvertiserGet()
-    {
-        return new AdReportAdvertiserGet($this->client);
-    }
-
     public function adReportAdGet()
     {
         return new AdReportAdGet($this->client);
     }
 
+    public function adReportAdMaterialGet()
+    {
+        return new AdReportAdMaterialGet($this->client);
+    }
+
+    public function adReportAdvertiserGet()
+    {
+        return new AdReportAdvertiserGet($this->client);
+    }
+
+    public function adReportCreativeGet()
+    {
+        return new AdReportCreativeGet($this->client);
+    }
+
+    public function adReportCustomConfigGet()
+    {
+        return new AdReportCustomConfigGet($this->client);
+    }
+
+    public function adReportCustomGet()
+    {
+        return new AdReportCustomGet($this->client);
+    }
+
+    public function adReportLongTransferOrderGet()
+    {
+        return new AdReportLongTransferOrderGet($this->client);
+    }
+
+    public function adReportMaterialGet()
+    {
+        return new AdReportMaterialGet($this->client);
+    }
+
+    public function adReportSearchWordGet()
+    {
+        return new AdReportSearchWordGet($this->client);
+    }
+
+    public function adReportUniPromotionDimensionDataAuthorGet()
+    {
+        return new AdReportUniPromotionDimensionDataAuthorGet($this->client);
+    }
+
+    public function adReportUniPromotionDimensionDataRoomGet()
+    {
+        return new AdReportUniPromotionDimensionDataRoomGet($this->client);
+    }
+
+    public function adReportUniPromotionGet()
+    {
+        return new AdReportUniPromotionGet($this->client);
+    }
+
+    public function adReportVideoUserLoseGet()
+    {
+        return new AdReportVideoUserLoseGet($this->client);
+    }
 }
