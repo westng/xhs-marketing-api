552860a8c66702b44bcb273ee316a0639f8997be
west_ng <457395070@qq.com>
2024-07-16 14:46:10 +0800

    更新【广告数据报表】API

---
+++ DataReport/AdReport/AdReportCreativeGet.php
@@ -0,0 +1,56 @@
+<?php 
+/*
+ * @Author: west_ng 457395070@qq.com
+ * @Date: 2024-07-15 21:46:58
+ * @LastEditors: west_ng 457395070@qq.com
+ * @LastEditTime: 2024-07-16 14:35:51
+ * @FilePath: /oc-marketing-api/DataReport/AdReport/AdReportAdvertiserAdGet.php
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/**
+ * 获取广告创意数据
+ * 此接口用于获取广告创意数据
+ * 
+ * User: westng
+ * Date: 2024/6/15
+ * Time: 10:45
+ *
+ */
+
+namespace DataReport\AdReport;
+
+use core\Helper\RequestCheckUtil;
+use core\Profile\RpcRequest;
+
+class AdReportCreativeGet extends RpcRequest
+{
+    protected $url = 'https://ad.oceanengine.com/open_api/v1.0/qianchuan/report/creative/get/';
+    protected $method = 'GET';
+    protected $content_type = 'application/json';
+
+    /**
+     * 操作的广告主id
+     * @var int $advertiser_id
+     */
+    protected $advertiser_id;
+
+    /**
+     * @param mixed $args
+     * @return $this
+     */
+    public function setArgs($args)
+    {
+        foreach ($args as $key => $value) {
+            $this->params[$key] = $this->{$key} = $value;
+        }
+        return $this;
+    }
+
+    /**
+     * @throws \core\Exception\InvalidParamException
+     */
+    public function check()
+    {
+        RequestCheckUtil::checkNotNull($this->advertiser_id, 'advertiser_id');
+    }
+}
--- DataReport/AdReport/Module.php
+++ DataReport/AdReport/Module.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * @Author: west_ng 457395070@qq.com
+ * @Date: 2024-06-15 10:54:17
+ * @LastEditors: west_ng 457395070@qq.com
+ * @LastEditTime: 2024-07-15 21:49:57
+ * @FilePath: /oc-marketing-api/DataReport/AdReport/Module.php
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 
 namespace DataReport\AdReport;
 
@@ -9,6 +17,11 @@ class Module extends BaseModule
     public function adReportAdvertiserGet()
     {
         return new AdReportAdvertiserGet($this->client);
+    }
+
+    public function adReportAdGet()
+    {
+        return new AdReportAdGet($this->client);
     }
 
 }
